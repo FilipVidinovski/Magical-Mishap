15dd81557ab8219d9002a58c672d5b7954dcb802
FilipVidinovski <38265146+FilipVidinovski@users.noreply.github.com>
2022-06-01 14:05:28 +0200

    Added Bouncing Ball prototype
    Bouncing Ball is a test for drawing and moving a singular ball across the screen in straight lines. The object switches vertical direction when it hits the top or bottom and horizontal when it hits the left or right.

---
+++ Prototypes/Bouncing Ball/Prototype - Vertical Bouncing Ball/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype___Vertical_Bouncing_Ball
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Vector2 location = new Vector2();
+        Vector2 velocity = new Vector2();
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            location.X = this.Width / 2;
+            location.Y = this.Height / 2;
+            velocity.X = 1;
+            velocity.Y = 4;
+
+            this.DoubleBuffered = true;
+            Timer timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+            timer.Enabled = true;
+
+
+
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            location = location + velocity;
+            if (location.Y < 0 || location.Y > this.Height - 160)
+            {
+                velocity.Y *= -1;
+            }
+            if (location.X < 0 || location.X > this.Width - 140)
+            {
+                velocity.X *= -1;
+            }
+            e.Graphics.FillEllipse(Brushes.BlueViolet, location.X, location.Y, 120, 120);
+        }
+
+        
+    }
+}
