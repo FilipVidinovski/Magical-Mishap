c39a845afa4a84e1aaa37a0c035dbdce493dcfe0
FilipVidinovski <38265146+FilipVidinovski@users.noreply.github.com>
2022-06-04 16:17:45 +0200

    Added Movement + Shooting prototype
    Прототип на играта вклучувајќи движење и испукување стрели. Во споредба со "Movement + Bouncing Balls" прототипот, овој прототип нема проблеми со framerate-от. Можно е наместо со цртање објектите да се дефинираат со pictureBox објектот.

---
+++ Prototypes/Prototype - Movement + Shooting/Prototype - Movement + Shooting/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool moveLeft, moveRight;
+        int movementHorisontal = 10;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            pictureBox1.Top = this.Height - 100;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                shoot(pictureBox1.Left + 10);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (moveLeft && pictureBox1.Left > 0)
+            {
+                pictureBox1.Left -= movementHorisontal;
+            }
+            if (moveRight && pictureBox1.Left < this.Width - 140)
+            {
+                pictureBox1.Left += movementHorisontal;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+        }
+
+        public void shoot(int pozicija)
+        {
+            Strela shoot = new Strela();
+            shoot.strelaPoz = pozicija;
+            shoot.Bullet(this);
+        }
+    }
+}
---
+++ Prototypes/Prototype - Movement + Shooting/Prototype - Movement + Shooting/Strela.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Prototype___Movement___Shooting
+{
+
+    internal class Strela
+    {
+        PictureBox strela = new PictureBox();
+        Timer strelaTimer = new Timer();
+
+        public int strelaPoz;
+
+        public void Bullet(Form form)
+        {
+            strela.BackColor = System.Drawing.Color.Green;
+            strela.Size = new Size(20, 20);
+            strela.Tag = "strela";
+            strela.Left = strelaPoz;
+            strela.Top = Form1.ActiveForm.Height - 140;
+            strela.BringToFront();
+            form.Controls.Add(strela);
+
+            strelaTimer.Interval = 10;
+            strelaTimer.Tick += new EventHandler(strelaTimer_tick);
+            strelaTimer.Start();
+        }
+
+        public void strelaTimer_tick(object sender, EventArgs e)
+        {
+            strela.Top -= 10;
+
+            if (strela.Top < 0)
+            {
+                strelaTimer.Stop();
+                strelaTimer.Dispose();
+                strela.Dispose();
+                strelaTimer = null;
+                strela = null;
+            }
+        }
+    }
+}
