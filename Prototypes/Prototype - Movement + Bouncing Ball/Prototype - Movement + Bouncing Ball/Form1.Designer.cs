7986228dbd69aad01b30103716e5c48e9d113b41
FilipVidinovski <38265146+FilipVidinovski@users.noreply.github.com>
2022-06-04 10:38:09 +0200

    Added Movement + Bouncing Ball Prototype
    Комбинација од последните 2 прототипи. Со користење на само 1 тајмер имаше проблеми со цртање на двата објекти заедно. Со користење на 2 посебни тајмери има проблеми со framerate-от. Или е проблем со послаби процесори, или е проблем со методите користени за цртање. Потребно тестирање.

---
+++ Prototypes/Prototype - Movement + Bouncing Ball/Prototype - Movement + Bouncing Ball/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Prototype___Movement___Bouncing_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 392);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 60);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Prototype - Movement + Moving Ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Prototypes/Prototype - Movement + Bouncing Ball/Prototype - Movement + Bouncing Ball/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype___Movement___Bouncing_Ball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Vector2 location = new Vector2();
+        Vector2 velocity = new Vector2();
+
+        bool moveLeft, moveRight;
+        int movementHorisontal = 5;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+
+            location.X = this.Width / 2;
+            location.Y = this.Height - 600;
+            velocity.X = (float)2;
+            velocity.Y = 5;
+
+            pictureBox1.Top = this.Height - 100;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            location += velocity;
+            if (location.Y > this.Height - 160)
+            {
+                velocity.Y *= -1;
+            }
+            if (location.Y < this.Height - 160)
+            {
+                velocity.Y += (float)0.5;
+            }
+            if (location.X < 0 || location.X > this.Width - 140)
+            {
+                velocity.X *= -1;
+            }
+            e.Graphics.FillEllipse(Brushes.DarkRed, location.X, location.Y, 120, 120);
+
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (moveLeft && pictureBox1.Left > 0)
+            {
+                pictureBox1.Left -= movementHorisontal;
+            }
+            if (moveRight && pictureBox1.Left < this.Width - 140)
+            {
+                pictureBox1.Left += movementHorisontal;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+        }
+    }
+}
