b7e50368c12537c102047476df55c082a856cac7
FilipVidinovski <38265146+FilipVidinovski@users.noreply.github.com>
2022-06-05 10:04:34 +0200

    Added Prototype - Movement + Bouncing Ball contact
    Додадено прототип за тестирање контакт измеѓу играчот и топката. Истотака експериментирање со нови методи за дефинирање.

---
+++ Prototypes/Prototype - Movement + Bouncing Ball contact/Prototype - Movement + Bouncing Ball contact/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Prototype___Movement___Bouncing_Ball_contact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Location = new System.Drawing.Point(96, 97);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Location = new System.Drawing.Point(95, 185);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(100, 50);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Prototypes/Prototype - Movement + Bouncing Ball contact/Prototype - Movement + Bouncing Ball contact/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype___Movement___Bouncing_Ball_contact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Vector2 velocity = new Vector2();
+        Vector2 location = new Vector2();
+        bool moveLeft, moveRight;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            player.Size = new Size(40, 60);
+            player.BackColor = Color.Blue;
+            player.Top = this.Height - 100;
+            player.Left = this.Width / 2;
+
+            ball.Size = new Size(120, 120);
+            ball.BackColor = Color.DarkRed;
+            location.X = 100;
+            location.Y = this.Width / 2;
+            ball.Top = (int)location.X;
+            ball.Left = (int)location.Y;
+            
+
+
+            velocity.X = 10;
+            velocity.Y = 2;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            location.X = ball.Left;
+            location.Y = ball.Top;
+
+            if (location.X > this.Width - 140 || location.X < 0)
+            {
+                velocity.X *= -1;
+            }
+            if (location.Y < (this.Height - 170))
+            {
+                velocity.Y += 1;
+            }
+            else
+            {
+                velocity.Y *= -1;
+            }
+
+            location.X += velocity.X;
+            location.Y += velocity.Y;
+            ball.Top = (int)location.Y;
+            ball.Left = (int)location.X;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(moveLeft && player.Left > 0)
+            {
+                player.Left -= 12;
+            }
+            if(moveRight && player.Left < this.Width - 80)
+            {
+                player.Left += 12;
+            }
+            if (player.Bounds.IntersectsWith(ball.Bounds))
+            {
+                label1.Text = "Hit";
+            }
+            else
+            {
+                label1.Text = "Not hit";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+        }
+    }
+}
